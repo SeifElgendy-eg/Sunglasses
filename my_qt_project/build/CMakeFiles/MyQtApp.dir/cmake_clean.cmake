ed279f01acf554bc8b3f56fdf16d2d1cb5248100
SeifElgendy-eg <seifeldinelgendy5@gmail.com>
2025-09-23 03:24:47 +0300

    merged 2 new filters and the basic loading img code  using cli

---
+++ my_qt_project/build/CMakeFiles/MyQtApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyQtApp.dir/link.d"
+  "CMakeFiles/MyQtApp.dir/main.cpp.o"
+  "CMakeFiles/MyQtApp.dir/main.cpp.o.d"
+  "MyQtApp"
+  "MyQtApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyQtApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
