eb15903fe7818fbe8cda595d06279ae1cb6a2169
Nightshade <149253447+SeifElgendy-eg@users.noreply.github.com>
2025-10-30 21:16:00 +0300

    Delete my_qt_project directory

--- my_qt_project/build/.qt/IntroToQML_res.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "IntroToQML")
-set(working_dir "/home/shade/codingProjects/fcai/my_qt_project")
-set(src_and_dest_list
-    "/home/shade/codingProjects/fcai/my_qt_project/resources.qrc"
-    "/home/shade/codingProjects/fcai/my_qt_project/build/Main/resources.qrc"
-
-)
-set(timestamp_file "/home/shade/codingProjects/fcai/my_qt_project/build/.qt/IntroToQML_res.txt")
